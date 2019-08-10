9d8282d009001e41222f854f18d85df9dc7dd7c1
Tomek <yjedi@wp.pl>
2019-08-10 12:12:45 +0200

    Nogi

---
+++ Nogi.java
@@ -0,0 +1,13 @@
+package Example;
+
+public class Nogi {
+	public static void main(String[] args) {
+		System.out.println(farma(2,4,6));
+	}
+
+	private static int farma(int a, int b, int c) {
+		return a*2+4*b+4*c;
+		
+	}
+	
+}
