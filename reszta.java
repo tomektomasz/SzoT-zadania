719ae9ac5fb24d167c19ea98f01d3707db037ab1
Tomek <yjedi@wp.pl>
2019-08-10 12:09:52 +0200

    reszta
    reszta

---
+++ reszta.java
@@ -0,0 +1,13 @@
+package Example;
+
+public class reszta {
+	public static void main(String[] args) {
+
+		System.out.println("reszta: " + reszta(10,4));
+	}
+
+	private static int reszta(int a, int b) {
+		return a%b ;
+		
+	}
+}
