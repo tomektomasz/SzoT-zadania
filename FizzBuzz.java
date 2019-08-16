1f449eb3bb3f345ca2ff09e09794d81e4fffa73d
Tomek <yjedi@wp.pl>
2019-08-16 14:17:14 +0200

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,16 +2,17 @@ package Example;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
-		potega(16);
+		fizzB(16);
 	}
 
-	private static void potega(int n) {
+	private static void fizzB(int n) {
 		//System.out.println("asfaf");
 		for(int i=1;i<=n; i++) {
+			System.out.print(i+" - ");
 			if(i%3==0&&i%5==0) System.out.println("FizzBuzz");
 			else if(i%3==0) System.out.println("Fizz");
 			else if(i%5==0) System.out.println("Buzz");
-			else System.out.println(i);			
+			else System.out.println(" ");			 
 		}
 		
 	}
